730186a606b956cf65808478d5437e207d78df7c
TauanGB <tauan.zaque@gmail.com>
2026-02-26 11:01:54 -0300

    Fixing a big mistake on the call of at a env var

--- src/services/httpclient.ts
+++ src/services/httpclient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { errorInterceptor, responseInterceptor } from './interceptors';
 
 const httpClient = axios.create({
-	baseURL: process.env.API_BASE_URL,
+	baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 	headers: {
 		'Content-Type': 'application/json',
 	},
